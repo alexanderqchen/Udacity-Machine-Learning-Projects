a77dae74c8994e660bcfe9f9e1cf5185f62b5649
alexanderqchen <room2210@yahoo.com>
2017-08-08 23:10:56 -0700

    did choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -28,9 +28,15 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
-
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print "calculating accuracy"
+print accuracy_score(pred, labels_test)
 
 
 
@@ -42,3 +48,5 @@ try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+print "done"
